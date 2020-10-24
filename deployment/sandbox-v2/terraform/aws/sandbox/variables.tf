ab93a9bb8dd264b00944a31ad6463697f0da6105
av <ankitvaishnav.iitb1991@gmail.com>
2020-10-24 18:50:49 +0530

    1.1.2 updates

--- deployment/sandbox-v2/terraform/aws/sandbox/variables.tf
+++ deployment/sandbox-v2/terraform/aws/sandbox/variables.tf
@@ -1,5 +1,5 @@
 variable "sandbox_name" { //Change this to your sandbox name.
-  default = "apple"  // This is informational.  A tag will be added with this name.
+  default = "hosur"  // This is informational.  A tag will be added with this name.
 }
 
 variable "region" {
